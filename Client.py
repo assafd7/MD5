e4077bbb3ca1b91bda82f255c03121e3583b1ad0
assaf dor <assafdor5@gmail.com>
2024-11-05 22:22:22 +0200

    updated finished files

--- Client.py
+++ Client.py
@@ -1,16 +1,15 @@
 import socket
 import hashlib
-import base64
 
 MD5TODEC = "202cb962ac59075b964b07152d234b70".lower()
 
-
 def send(msg, client_socket):
+    # Send message with length header
     final_msg = str(len(msg)) + "#" + msg
     client_socket.send(final_msg.encode())
 
-
 def recv(client_socket):
+    # Receive message with length header
     msg_len = ''
     msg = ''
     char = client_socket.recv(1).decode()
@@ -21,26 +20,30 @@ def recv(client_socket):
         msg += client_socket.recv(1).decode()
     return msg
 
-
 def get_md5_of_string(input_string):
+    # Calculate MD5 hash
     return hashlib.md5(input_string.encode()).hexdigest()
-
 
 def start_client(host='127.0.0.1', port=8080):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((host, port))
-        data = recv(client_socket)
-        data = int(data)
-        print(f"Received back: {data}")
-        ranged = [data, data + 10]
-        for a in range(ranged[0], ranged[1]):
-            result = get_md5_of_string(str(a))
-            print(result)
-            if result == MD5TODEC:
-                print("got it")
-                send("found", client_socket)
+        while True:
+            data = recv(client_socket)
+            if data == "found":
+                print("Stopping search, found by another client.")
                 break
 
+            start = int(data)
+            end = start + 10
+            for a in range(start, end):
+                result = get_md5_of_string(str(a))
+                print(f"Checking {a}: {result}")
+                if result == MD5TODEC:
+                    print("Match found!")
+                    send("found", client_socket)
+                    return  # Stop after finding the result
+
+            send("next", client_socket)
 
 if __name__ == "__main__":
     start_client()
--- Server.py
+++ Server.py
@@ -5,17 +5,17 @@ QUEUE_SIZE = 10
 IP = '0.0.0.0'
 PORT = 8080
 ranges = 100
-
+found = False
 
 def send(msg, client_socket):
+    # Send message with length header
     final_msg = str(len(msg)) + "#" + msg
     client_socket.send(final_msg.encode())
 
-
 def recv(client_socket):
+    # Receive message with length header
     msg_len = ''
     msg = ''
-    print(client_socket)
     char = client_socket.recv(1).decode()
     while char != "#":
         msg_len += char
@@ -24,48 +24,48 @@ def recv(client_socket):
         msg += client_socket.recv(1).decode()
     return msg
 
-
 def handle_connection(client_socket, client_address):
-    """
-   handle a connection
-   :param client_socket: the connection socket
-   :param client_address: the remote address
-   :return: None
-   """
-    global ranges
+    # handles each client connection
+    global ranges, found
     try:
-        print('New connection received from ' + client_address[0] + ':' + str(client_address[1]))
-        send(str(ranges), client_socket)
-        if recv(client_socket) == "found":
-            print("found")
+        print(f'New connection from {client_address[0]}:{client_address[1]}')
+
+        while not found:
+            send(str(ranges), client_socket)
+            ranges += 10
+
+            response = recv(client_socket)
+            if response == "found":
+                found = True
+                print("Hash found, notifying all clients.")
+                break
+            elif response != "next":
+                print(f"Unexpected response: {response}")
+
+        send("found", client_socket)
 
     except socket.error as err:
-        print('received socket exception - ' + str(err))
+        print(f"Socket error - {err}")
     finally:
         client_socket.close()
 
-
 def main():
     global ranges
-    # Open a socket and loop forever while waiting for clients
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         server_socket.bind((IP, PORT))
         server_socket.listen(QUEUE_SIZE)
-        print("Listening for connections on port %d" % PORT)
+        print(f"Listening on port {PORT}")
 
         while True:
             client_socket, client_address = server_socket.accept()
-            thread = Thread(target=handle_connection,
-                            args=(client_socket, client_address))
+            thread = Thread(target=handle_connection, args=(client_socket, client_address))
             thread.start()
-            ranges += 10
+
     except socket.error as err:
-        print('received socket exception - ' + str(err))
+        print(f"Socket error - {err}")
     finally:
         server_socket.close()
 
-
 if __name__ == "__main__":
-    # Call the main handler function
     main()
